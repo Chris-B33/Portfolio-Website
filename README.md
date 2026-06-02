0a0116f3505081f27af2cd6c8a598f2bcce0dfa7
Chris-B33 <brophy.c03@gmail.com>
2026-06-02 19:34:50 +0100

    Initial website layout ready.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Portfolio-Website
-Website for displaying past projects
---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+        title: "Sorting Algorithm Visualiser",
+        slug: "sav",
+        tags: ["JavaScript"],
+        description: "Sorting algorithm visualizer"
+    },
+    {
+        title: "Brophy's Game of Life",
+        slug: "bgol",
+        tags: ["Python"],
+        description: "Browser Game of Life"
+    },
+    {
+        title: "Federated Imitation Learning",
+        slug: "fil",
+        tags: ["Python"],
+        description: "Mario Kart Wii Federated Imitation Learning"
+    },
+    {
+        title: "Reinforcement Racing Model",
+        slug: "rrm",
+        tags: ["Lua"],
+        description: "Mario Kart DS Reinforcement Learning"
+    }
+];
